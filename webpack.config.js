73d3c433a40e2879331b2880cb68ae8f64858f17
Ahmad Asaad <ahmadasaad89@users.noreply.github.com>
2018-12-05 14:17:47 +0100

    React webpack devstack starting boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.join(__dirname, "/dist"),
+        filename: "index.bundle.js"
+    },
+    devServer: {
+        stats: {
+            children: false,
+            maxModules: 0
+        },
+        port: 3001
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./index.html"
+        })
+    ]
+};
